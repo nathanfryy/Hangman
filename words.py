4c82493a5a40ce783b9f89d18985f1abd9b755ab
Nathan Fry <62443031+nathanfryy@users.noreply.github.com>
2020-05-10 12:38:22 -0500

    Update words.py

--- words.py
+++ words.py
@@ -1,1 +1,8 @@
-wordList = ['cat', 'dog', 'meow']
+wordList1 = ['cat', 'dog', 'meow', 'fish', 'hamster',
+'bark', 'lizard', 'goat', 'sheep', 'cow']
+
+wordList2 = ['apple', 'pizza', 'cake', 'carrot', 'grape',
+'banana', 'pie', 'brownie', 'chips', 'cookie']
+
+wordList3 = ['baseball', 'volleyball', 'soccer', 'football',
+'croquet', 'basketball', 'rugby', 'golf', 'track', 'wrestling']
