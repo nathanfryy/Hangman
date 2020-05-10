8fe62ea643092b5af084119c3a018ee67b71b30d
Ben37123 <61328829+Ben37123@users.noreply.github.com>
2020-05-10 10:14:26 -0700

    Update player.py

--- player.py
+++ player.py
@@ -7,7 +7,10 @@ class Player(object):
 
     def guess(self):
         letter = toolbox.get_string('What is your letter guess?')
-        if len(letter) > 1:
-            letter = toolbox.get_string('What is your letter guess? Has to be one character.')
-        if len(letter) < 1:
-            letter = toolbox.get_string('What is your letter guess? Has to be one character.')
+        if len(letter) != 1:
+            print('Your guess has to be one character.')
+            self.guess()
+        if letter == '1' or letter == '2' or letter == '3' or letter == '4' or letter == '5' or letter == '6' or letter == '7' or letter == '8' or letter == '9' or letter == '0':
+            print('Your guess has to be a letter.')
+            self.guess()
+        return letter
